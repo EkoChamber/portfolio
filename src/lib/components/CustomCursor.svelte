044c9115f0a6cdb750b6de7108342d36caf2a077
EkoChamber <aadelfin1@up.edu.ph>
2025-03-19 01:34:23 +0800

    Hide custom cursor for mobile

--- src/lib/components/CustomCursor.svelte
+++ src/lib/components/CustomCursor.svelte
@@ -4,10 +4,42 @@
     let x = 0;
     let y = 0;
     let isHovering = false;
+    let isMobile = false;
+    let cursorVisible = true;
+
+    function checkMobile(): boolean {
+        return (
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || 
+            (window.matchMedia("(max-width: 768px)").matches)
+        );
+    }
 
     function handleMouseMove(e: MouseEvent): void {
         x = e.clientX;
         y = e.clientY;
+        cursorVisible = true;
+    }
+
+    function handleTouchMove(e: TouchEvent): void {
+        if (e.touches.length > 0) {
+            x = e.touches[0].clientX;
+            y = e.touches[0].clientY;
+            cursorVisible = true;
+        }
+    }
+
+    function handleTouchStart(e: TouchEvent): void {
+        if (e.touches.length > 0) {
+            x = e.touches[0].clientX;
+            y = e.touches[0].clientY;
+            cursorVisible = true;
+        }
+    }
+
+    function handleTouchEnd(): void {
+        if (isMobile) {
+            cursorVisible = false;
+        }
     }
 
     function handleMouseOver(e: MouseEvent): void {
@@ -26,21 +58,36 @@
     }
 
     onMount(() => {
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('mouseover', handleMouseOver);
-        window.addEventListener('mouseout', handleMouseOut);
+        isMobile = checkMobile();
+
+        if (!isMobile) {
+            window.addEventListener('mousemove', handleMouseMove);
+            window.addEventListener('mouseover', handleMouseOver);
+            window.addEventListener('mouseout', handleMouseOut);
+        } else {
+            window.addEventListener('touchmove', handleTouchMove);
+            window.addEventListener('touchstart', handleTouchStart);
+            window.addEventListener('touchend', handleTouchEnd);
+            
+            cursorVisible = false;
+        }
 
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
             window.removeEventListener('mouseover', handleMouseOver);
             window.removeEventListener('mouseout', handleMouseOut);
+            window.removeEventListener('touchmove', handleTouchMove);
+            window.removeEventListener('touchstart', handleTouchStart);
+            window.removeEventListener('touchend', handleTouchEnd);
         };
     });
 </script>
   
+{#if cursorVisible}
 <div 
+    class="cursor"
+    class:hovering={isHovering}
     style="
-        position: fixed;
         left: {x}px;
         top: {y}px;
         width: 10px;
@@ -50,10 +97,16 @@
         z-index: 9999;
         transform: translate(-50%, -50%);"
 ></div>
+{/if}
   
 <style>
-    div {
+    .cursor {
         position: fixed;
         pointer-events: none;
+        transition: transform 0.1s ease;
+    }
+    
+    .hovering {
+        transform: translate(-50%, -50%) scale(1.5);
     }
 </style>
