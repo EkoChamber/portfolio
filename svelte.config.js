ccd5ced30615788728f2830b6f35821934c44a37
EkoChamber <aadelfin1@up.edu.ph>
2025-03-19 00:21:08 +0800

    Add GitHub Pages deployment workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
+const repo = 'portfolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    }),
+    paths: {
+      base: dev ? '' : '/' + repo
+    }
+  }
 };
 
 export default config;
